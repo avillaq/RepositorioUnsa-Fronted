ea1f638e76ffd21402a258145c816673957f1eeb
avillaq <villafuertequispealex@gmail.com>
2024-11-10 22:29:54 -0500

    Se añade el componente Pagination para gestionar la paginación de documentos

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,21 @@
+
+export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePrevious = () => {
+    if (currentPage > 1) onPageChange(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) onPageChange(currentPage + 1);
+  };
+  return (
+    <div className="pagination">
+      <button onClick={handlePrevious} disabled={currentPage === 1}>
+        Anterior
+      </button>
+      <span>Página {currentPage} de {totalPages}</span>
+      <button onClick={handleNext} disabled={currentPage === totalPages}>
+        Siguiente
+      </button>
+    </div>
+  )
+}
