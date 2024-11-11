64dca42ef616f6e3d93758d3027c64afe4cccd42
avillaq <villafuertequispealex@gmail.com>
2024-11-11 11:55:22 -0500

    Implementacion de las rutas en el main

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home } from './components/Home';
-import { SearchResults } from './pages/SearchResults';
-import { Profile } from './pages/Profile';
-
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/resultados" element={<SearchResults />} />
-        <Route path="/perfil" element={<Profile />} />
-      </Routes>
-    </Router>
+    <div></div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './components/Home';
+import { SearchResults } from './pages/SearchResults';
+import { Profile } from './pages/Profile';
 import './styles/global.css';
-import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/resultados',
+    element: <SearchResults />,
+  },
+  {
+    path: '/perfil',
+    element: <Profile />,
+  }
+], {
+  future: {
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} future={{ v7_startTransition: true,}}
+/>
   </StrictMode>,
 )
