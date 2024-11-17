4f9bc5ef27566ffbe383fd2878e104bd146e6739
avillaq <villafuertequispealex@gmail.com>
2024-11-17 14:17:34 -0500

    Renombrar parámetro de búsqueda en la función searchDocuments para mayor claridad

--- src/services/api.js
+++ src/services/api.js
@@ -3,15 +3,11 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Funcion para realizar una busqueda de documentos
-export const searchDocuments = async (query, filters, currentPage = 1, pageSize = 10) => {
-  let ejemplo = [
-    { id: 1, title: "Introducción a IA en agricultura", authors: "A. Pérez, B. Gómez", editor: 1, fecha: 2021, resumen: "Estudio sobre aplicaciones de IA en la agricultura.", coleccion: "Ingeniería" }
-  ];
-
+export const searchDocuments = async (searchQuery, filters, currentPage = 1, pageSize = 10) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/documentos`, {
       params: {
-        title: query,
+        title: searchQuery,
         page: currentPage,
         limit: pageSize,
       }
