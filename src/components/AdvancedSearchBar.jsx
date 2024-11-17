b110c49d7e2500193fa652c1cbf799092d627c7f
avillaq <villafuertequispealex@gmail.com>
2024-11-17 13:12:24 -0500

    Actualizar opciones del componente AdvancedSearchBar y agregar manejo de placeholders dinámicos

--- src/components/AdvancedSearchBar.jsx
+++ src/components/AdvancedSearchBar.jsx
@@ -1,10 +1,11 @@
 import { useState, useId } from 'react';
 
-const options = ["Titulo", "Autor"];
+const options = ["Logica", "Lenguaje natural"];
 
 export const AdvancedSearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState('');
   const [selected, setSelected] = useState(options[0]);
+  const [placeholder, setPlaceholder] = useState("Ejemplo: Pedagogia and Educacion");
   
   const dropdownButtonId = useId();
   const dropdownId = useId();
@@ -19,6 +20,15 @@ export const AdvancedSearchBar = ({ onSearch }) => {
     onSearch(query);
   };
 
+  const handleSelected = (option) => {
+    setSelected(option);
+    if (option === "Logica") {
+      setPlaceholder("Ej. Pedagogia and Educacion");
+    } else {
+      setPlaceholder("Ej. ¿Cuales son los beneficios de la educacion a distancia?");
+    }
+  };
+
   return (
     <form className="max-w-lg mx-auto" onSubmit={handleSearch}>
       <div className="flex">
@@ -31,14 +41,14 @@ export const AdvancedSearchBar = ({ onSearch }) => {
             {
               options.map((option, index) => (
                 <li key={index}>
-                  <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100" onClick={() => setSelected(option)}>{option}</button>
+                  <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100" onClick={() => handleSelected(option)}>{option}</button>
                 </li>
               ))
             }
           </ul>
         </div>
         <div className="relative w-full">
-          <input type="search" id={searchInputId} className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500" placeholder="Busca artículos o tesis aquí..." required value={query} onChange={handleInputChange} />
+          <input type="search" id={searchInputId} className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500" placeholder={placeholder} required value={query} onChange={handleInputChange} />
           <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
