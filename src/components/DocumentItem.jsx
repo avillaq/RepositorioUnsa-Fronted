127ca3156bf48aa34ff15c6150fef9a7171f63e8
avillaq <villafuertequispealex@gmail.com>
2024-11-16 22:08:22 -0500

    Actualizar el componente DocumentItem para incluir el campo 'faculty' y mejorar el estilo

--- src/components/DocumentItem.jsx
+++ src/components/DocumentItem.jsx
@@ -1,8 +1,9 @@
 export const DocumentItem = ({ id, title, authors, editor, year, faculty, onClick }) => {
   return (
-    <div onClick={onClick} className="py-3 px-6 mb-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-      <h3 className="text-xl font-semibold text-gray-800 mb-2 text-left">{title}</h3>
-      <p className="text-sm text-gray-600 text-left">({editor}, {year}) {authors}</p>
+    <div onClick={onClick} className="py-4 px-6 rounded-lg border-b-2 last:border-b-0 cursor-pointer hover:bg-gray-100 transition-colors duration-300">
+      <p className="text-sm text-gray-500">{faculty}</p>
+      <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
+      <p className="text-sm text-gray-600">({editor}, {year}) {authors}</p>
     </div>
   )
 }
