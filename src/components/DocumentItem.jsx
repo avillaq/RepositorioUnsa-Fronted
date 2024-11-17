d9302901d6526b2ad87c30f981bfd915372b77fe
avillaq <villafuertequispealex@gmail.com>
2024-11-16 21:57:29 -0500

    Agregar campo 'faculty' al componente DocumentItem para incluir información adicional

--- src/components/DocumentItem.jsx
+++ src/components/DocumentItem.jsx
@@ -1,4 +1,4 @@
-export const DocumentItem = ({ title, authors, editor, year, onClick }) => {
+export const DocumentItem = ({ id, title, authors, editor, year, faculty, onClick }) => {
   return (
     <div onClick={onClick} className="py-3 px-6 mb-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
       <h3 className="text-xl font-semibold text-gray-800 mb-2 text-left">{title}</h3>
