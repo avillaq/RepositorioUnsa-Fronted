92855d3d63fb5f9be70e0ca74844d41caf609fb9
avillaq <villafuertequispealex@gmail.com>
2024-11-17 14:01:39 -0500

    Refactor DocumentItem component to use a document object and update displayed fields

--- src/components/DocumentItem.jsx
+++ src/components/DocumentItem.jsx
@@ -1,9 +1,10 @@
-export const DocumentItem = ({ id, title, authors, editor, year, faculty, onClick }) => {
+export const DocumentItem = ({ document }) => {
+  const { id, titulo, id_editor, fecha, id_coleccion } = document
   return (
-    <div onClick={onClick} className="py-4 px-6 rounded-lg border-b-2 last:border-b-0 cursor-pointer hover:bg-gray-100 transition-colors duration-300" id={id}>
-      <p className="text-sm text-gray-500">{faculty}</p>
-      <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
-      <p className="text-sm text-gray-600">({editor}, {year}) {authors}</p>
+    <div className="py-4 px-6 rounded-lg border-b-2 last:border-b-0 cursor-pointer hover:bg-gray-100 transition-colors duration-300" id={id}>
+      <p className="text-sm text-gray-500">{id_coleccion}</p>
+      <h3 className="text-xl font-semibold text-gray-800 mb-2">{titulo}</h3>
+      <p className="text-sm text-gray-600">({"Universidad Nacional de San Agustin"}, {fecha}) {"Juanito Perez"}</p>
     </div>
   )
 }
