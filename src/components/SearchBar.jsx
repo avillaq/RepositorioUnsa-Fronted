7f91a920a61b723f69c5d7785e1177aac7cbc5ee
avillaq <villafuertequispealex@gmail.com>
2024-11-17 12:57:33 -0500

    Agregar identificadores únicos a los elementos del componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useState, useId } from 'react';
 
-export const SearchBar = ({ onSearch , options}) => {
+const options = ["Titulo", "Autor"];
+
+export const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState('');
   const [selected, setSelected] = useState(options[0]);
+  
+  const dropdownButtonId = useId();
+  const dropdownId = useId();
+  const searchInputId = useId();
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
@@ -10,18 +16,18 @@ export const SearchBar = ({ onSearch , options}) => {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    onSearch(query); 
+    onSearch(query);
   };
 
   return (
     <form className="max-w-lg mx-auto" onSubmit={handleSearch}>
       <div className="flex">
-        <button id="dropdown-button" data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">{selected}<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <button id={dropdownButtonId} data-dropdown-toggle={dropdownId} className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">{selected}<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
         </svg>
         </button>
-        <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
-          <ul className="py-2 text-sm text-gray-700" aria-labelledby="dropdown-button">
+        <div id={dropdownId} className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+          <ul className="py-2 text-sm text-gray-700" aria-labelledby={dropdownButtonId}>
             {
               options.map((option, index) => (
                 <li key={index}>
@@ -32,7 +38,7 @@ export const SearchBar = ({ onSearch , options}) => {
           </ul>
         </div>
         <div className="relative w-full">
-          <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500" placeholder="Busca artículos o tesis aquí..." required value={query} onChange={handleInputChange} />
+          <input type="search" id={searchInputId} className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500" placeholder="Busca artículos o tesis aquí..." required value={query} onChange={handleInputChange} />
           <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
