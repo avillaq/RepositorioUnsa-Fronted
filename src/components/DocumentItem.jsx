bb15fd043ff362306967109698e0dce1e776acb0
avillaq <villafuertequispealex@gmail.com>
2024-11-16 20:42:22 -0500

    Actualizar estilo del componente DocumentItem para mejorar la presentación de título, editor y año

--- src/components/DocumentItem.jsx
+++ src/components/DocumentItem.jsx
@@ -1,8 +1,8 @@
 export const DocumentItem = ({ title, authors, editor, year, onClick }) => {
   return (
-    <div onClick={onClick} className="p-4 mb-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-      <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
-      <p className="text-sm text-gray-600 mb-1">({editor}, {year}) {authors}</p>
+    <div onClick={onClick} className="py-3 px-6 mb-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <h3 className="text-xl font-semibold text-gray-800 mb-2 text-left">{title}</h3>
+      <p className="text-sm text-gray-600 text-left">({editor}, {year}) {authors}</p>
     </div>
   )
 }
